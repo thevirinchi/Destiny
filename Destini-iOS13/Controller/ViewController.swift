29be52ff214092821fc9fbb50299189168145406
z3t4z00k <zetazook@gmail.com>
2020-10-18 11:06:00 +0530

    Added functions to update UI

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -6,9 +6,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    var story:Story?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        story = Story(story: storyBrain.getStory())
+        storyLabel.text = story?.title
+        choice1Button.setTitle(story?.choi1, for: .normal)
+        choice2Button.setTitle(story?.choi2, for: .normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if(!storyBrain.proceed(title: sender.currentTitle!)){
+            storyLabel.text = "The End."
+        }
+        else{
+            Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        }
+    }
+    
+    @objc func updateUI(){
+        story = Story(story: storyBrain.getStory())
+        storyLabel.text = story?.title
+        choice1Button.setTitle(story?.choi1, for: .normal)
+        choice2Button.setTitle(story?.choi2, for: .normal)
     }
 
 
