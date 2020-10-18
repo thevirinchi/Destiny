049b5042d1ddcb129f3ae75b61d0fc8ee54fda5a
z3t4z00k <zetazook@gmail.com>
2020-10-18 10:15:16 +0530

    Created a structure to store Story

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -1,1 +1,17 @@
 import Foundation
+
+struct Story{
+    let title: String
+    let choi1: String
+    let choi2: String
+    let choi1Des: Int
+    let choi2Des: Int
+    
+    init(title:String, choi1:String, choi2:String, choi1Des:Int, choi2Des:Int) {
+        self.title = title
+        self.choi1 = choi1
+        self.choi2 = choi2
+        self.choi1Des = choi1Des
+        self.choi2Des = choi2Des
+    }
+}
