ad6e733e00e6f0d568be808cfb0983d00781e029
z3t4z00k <zetazook@gmail.com>
2020-10-18 11:05:41 +0530

    Added another init function

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,11 +7,19 @@ struct Story{
     let choi1Des: Int
     let choi2Des: Int
     
-    init(title:String, choi1:String, choi2:String, choi1Des:Int, choi2Des:Int) {
+    init(title:String, choi1:String, choi1Des:Int, choi2:String, choi2Des:Int) {
         self.title = title
         self.choi1 = choi1
         self.choi2 = choi2
         self.choi1Des = choi1Des
         self.choi2Des = choi2Des
+    }
+    
+    init(story: Story) {
+        self.title = story.title
+        self.choi1 = story.choi1
+        self.choi2 = story.choi2
+        self.choi1Des = story.choi1Des
+        self.choi2Des = story.choi2Des
     }
 }
